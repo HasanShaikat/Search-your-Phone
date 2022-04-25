3570ec441ffaad140d5f935b444f5a72987e0093
HasanShaikat <hasanshaikat9@gmail.com>
2022-04-25 20:50:58 +0600

    empty search error fix

--- js/app.js
+++ js/app.js
@@ -14,17 +14,27 @@ const search = () =>{
 
 const displayPhone = phones => {
     const displayPhones = document.getElementById('display-phones');
-    displayPhones.textContent = '';
+    if(phones == ''){
+        
+        displayPhones.textContent = '';
+        alert('Not Found Phones!')
+    }else{
+        // const displayPhones = document.getElementById('display-phones');
+        displayPhones.textContent = '';
 
-    phones.forEach(phone => {
-        const div = document.createElement('div');
-        div.classList.add()
-        div.innerHTML = `
-            <img src=${phone.image} class="shadow-lg">
-            <h2>Brands: ${phone.brand}</h2>    
-            <h2>Name: ${phone.phone_name}</h2>    
-        `
+        phones.forEach(phone => {
+            const div = document.createElement('div');
+            div.classList.add('mx-auto','shadow-lg', 'mb-2','rounded','w-3/4',)
+            div.innerHTML = `
+                <img src=${phone.image} class="mx-auto">
+                <div class="py-4">
+                    <h2> <span class="text-green-400">Brands:</span> ${phone.brand}</h2>    
+                    <h2>Name: ${phone.phone_name}</h2>
+                    <button onclick="phoneDetail()">Detail</button>
+                </div>   
+            `
 
         displayPhones.appendChild(div);
-    })
+        })
+    }
 }
