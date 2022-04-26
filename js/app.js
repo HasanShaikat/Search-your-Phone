77faf92959117062dbd906556c664f9b172151fa
HasanShaikat <hasanshaikat9@gmail.com>
2022-04-26 13:36:37 +0600

    Single Phone Display

--- js/app.js
+++ js/app.js
@@ -28,13 +28,42 @@ const displayPhone = phones => {
             div.innerHTML = `
                 <img src=${phone.image} class="mx-auto">
                 <div class="py-4">
-                    <h2> <span class="text-green-400">Brands:</span> ${phone.brand}</h2>    
-                    <h2>Name: ${phone.phone_name}</h2>
-                    <button onclick="phoneDetail()">Detail</button>
+                    <h2> <span class="text-green-400 font-semibold">Brands : </span> ${phone.brand}</h2>    
+                    <h2><span class="text-green-400 font-semibold">Name : </span> ${phone.phone_name}</h2>
+                    <button class="bg-green-400 py-2 px-12 rounded mt-4 text-white" onclick="phoneDetail('${phone.slug}')">Detail</button>
                 </div>   
             `
-
         displayPhones.appendChild(div);
         })
     }
+}
+
+const phoneDetail=id=>{
+    const url = `https://openapi.programming-hero.com/api/phone/${id}`;
+    fetch (url)
+    .then(res => res.json())
+    .then(data => displayPhoneDetails(data.data))
+}
+
+const displayPhoneDetails = phoneId =>{
+    const displaySinglePhone = document.getElementById('display-single-phone');
+
+    if(phoneId == ''){
+        displaySinglePhone.textContent = '';
+        alert('Not Found Phones!')
+    }else{
+        displaySinglePhone.textContent = '';
+    const div = document.createElement('div');
+    div.classList.add('mx-auto','border-x-4','border-indigo-500','rounded', 'w-full')
+    div.innerHTML = `
+        <img src=${phoneId.image} class="mx-auto">
+        <div class="py-4">
+            <h2> <span class="text-green-400 font-semibold">Brands : </span> ${phoneId.brand}</h2>    
+            <h2><span class="text-green-400 font-semibold">Name : </span> ${phoneId.name}</h2>
+        </div>
+    `
+    displaySinglePhone.appendChild(div)
+    }
+
+    
 }
